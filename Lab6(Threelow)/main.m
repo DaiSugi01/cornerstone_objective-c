6ef0bae4ad61fbb5947241abcec973f2001fa7cc
Daisugi <volble124@gmail.com>
2021-02-26 17:20:35 -0800

    initial commit

---
+++ Lab6(Threelow)/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Lab6(Threelow)
+//
+//  Created by 杉原大貴 on 2021/02/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+    }
+    return 0;
+}
