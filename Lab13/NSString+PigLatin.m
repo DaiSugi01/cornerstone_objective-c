d847fc8e04cde25cbb8d2d0714c713378ac62921
Daisugi <volble124@gmail.com>
2021-02-28 12:48:09 -0800

    modified lab13

--- Lab13/NSString+PigLatin.m
+++ Lab13/NSString+PigLatin.m
@@ -12,18 +12,10 @@
 -(NSString *)stringByPigLatinization {
     
     NSMutableString *result = [NSMutableString string];
-    
     [[self componentsSeparatedByString:@" "] enumerateObjectsUsingBlock:^(id word, NSUInteger idx, BOOL *stop) {
         [result appendString:[@[@"a", @"e", @"i", @"o", @"u", @"A", @"E", @"I", @"O", @"U"] containsObject:[word substringToIndex:1]] ? [NSString stringWithFormat:@"%@%@ay ", [[word substringToIndex:1] uppercaseString], [word substringFromIndex:1]] : [@[@"Ch", @"Sh", @"Sm", @"St", @"Th", @"Ps", @"Ph", @"Pl", @"Gl", @"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl", @"CH", @"SH", @"SM", @"ST", @"TH", @"PS", @"PH", @"PL", @"GL", @"cH", @"sH", @"sM", @"sT", @"tH", @"pS", @"pH", @"pL", @"gL"] containsObject:[word substringToIndex:2]] ? [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:2]] uppercaseString], [word substringFromIndex:3], [[word substringToIndex:2] lowercaseString]] : [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:1]] uppercaseString], [word substringFromIndex:2], [[word substringToIndex:1] lowercaseString]]];
     }];
     return result;
-    
-
-//    return [@[@"a", @"e", @"i", @"o", @"u", @"A", @"E", @"I", @"O", @"U"] containsObject:[self substringToIndex:1]] ? [NSString stringWithFormat:@"%@%@ay", [[self substringToIndex:1] uppercaseString], [self substringFromIndex:1]] : [@[@"Ch", @"Sh", @"Sm", @"St", @"Th", @"Ps", @"Ph", @"Pl", @"Gl", @"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl", @"CH", @"SH", @"SM", @"ST", @"TH", @"PS", @"PH", @"PL", @"GL", @"cH", @"sH", @"sM", @"sT", @"tH", @"pS", @"pH", @"pL", @"gL"] containsObject:[self substringToIndex:2]] ? [NSString stringWithFormat:@"%@%@%@ay", [[NSString stringWithFormat:@"%C", [self characterAtIndex:2]] uppercaseString], [self substringFromIndex:3], [[self substringToIndex:2] lowercaseString]] : [NSString stringWithFormat:@"%@%@%@ay", [[NSString stringWithFormat:@"%C", [self characterAtIndex:1]] uppercaseString], [self substringFromIndex:2], [[self substringToIndex:1] lowercaseString]] ;
-    
-    //    for (NSString *word in [self componentsSeparatedByString:@" "]) {
-    //        [result appendString:[@[@"a", @"e", @"i", @"o", @"u", @"A", @"E", @"I", @"O", @"U"] containsObject:[word substringToIndex:1]] ? [NSString stringWithFormat:@"%@%@ay ", [[word substringToIndex:1] uppercaseString], [word substringFromIndex:1]] : [@[@"Ch", @"Sh", @"Sm", @"St", @"Th", @"Ps", @"Ph", @"Pl", @"Gl", @"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl", @"CH", @"SH", @"SM", @"ST", @"TH", @"PS", @"PH", @"PL", @"GL", @"cH", @"sH", @"sM", @"sT", @"tH", @"pS", @"pH", @"pL", @"gL"] containsObject:[word substringToIndex:2]] ? [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:2]] uppercaseString], [word substringFromIndex:3], [[word substringToIndex:2] lowercaseString]] : [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:1]] uppercaseString], [word substringFromIndex:2], [[word substringToIndex:1] lowercaseString]]];
-    //    }
 }
 
 @end
