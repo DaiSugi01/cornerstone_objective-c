8b3567a760cb4ecaaba220b0bd083f63dfae9efc
Daisugi <volble124@gmail.com>
2021-03-02 15:04:10 -0800

    add lab13

--- Lab13/NSString+PigLatin.m
+++ Lab13/NSString+PigLatin.m
@@ -14,10 +14,10 @@
     NSMutableString *result = [NSMutableString string];
     
     for (NSString *word in [self componentsSeparatedByString:@" "]) {
-        [result appendString:[[self componentsSeparatedByString:@" "] count] == 1 ? [NSString stringWithFormat:@"%@ay", self] : [@[@"a", @"e", @"i", @"o", @"u"] containsObject:[[word substringToIndex:1] lowercaseString]] ? [NSString stringWithFormat:@"%@%@ay ", [[word substringToIndex:1] uppercaseString], [[word substringFromIndex:1] lowercaseString]] : [@[@"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl"] containsObject:[[word substringToIndex:2] lowercaseString]] ? [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:2]] uppercaseString], [word substringFromIndex:3], [[word substringToIndex:2] lowercaseString]] : [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:1]] uppercaseString], [word substringFromIndex:2], [[word substringToIndex:1] lowercaseString]]];
+        [result appendString:[word length] == 1 ? [NSString stringWithFormat:@"%@ay ", [word uppercaseString]] : [@[@"a", @"e", @"i", @"o", @"u"] containsObject:[[word substringToIndex:1] lowercaseString]] ? [NSString stringWithFormat:@"%@%@ay ", [[word substringToIndex:1] uppercaseString], [[word substringFromIndex:1] lowercaseString]] : [@[@"ch", @"sh", @"sm", @"st", @"th", @"ps", @"ph", @"pl", @"gl"] containsObject:[[word substringToIndex:2] lowercaseString]] ? [word length] == 2 ? [NSString stringWithFormat:@"%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:0]] uppercaseString], [NSString stringWithFormat:@"%C", [word characterAtIndex:1]] ]: [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:2]] uppercaseString], [word substringFromIndex:3], [[word substringToIndex:2] lowercaseString]] : [NSString stringWithFormat:@"%@%@%@ay ", [[NSString stringWithFormat:@"%C", [word characterAtIndex:1]] uppercaseString], [word substringFromIndex:2], [[word substringToIndex:1] lowercaseString]]];
     }
     
-    return result;
+    return [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
 
 @end
--- Lab13/main.m
+++ Lab13/main.m
@@ -10,7 +10,7 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSString *str = @"Kale Chips af";
+        NSString *str = @"Kale Chips af b ch a";
         NSLog(@"%@", [str stringByPigLatinization]);
     }
     return 0;
